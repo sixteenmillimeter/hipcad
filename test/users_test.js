67867c6b5c5630e9a6ddb7c88b183a445d44bbb2
Matt <dev@Works-Mac-Pro.local>
2015-08-07 12:57:41 -0400

    Fix db exists and add logger

--- test/users_test.js
+++ test/users_test.js
@@ -1,6 +1,38 @@
 var users = require('../lib/users.js');
 
 setTimeout(function () {
+		//console.time('users.test');
+		//console.timeEnd('users.test');
+		/*
+		users.create('matt', 'mmcwilliams@aspectart.org', 'test', function (obj) {
+			console.dir(obj);
+			users.exists('matt', function (exists) {
+				console.log(exists);
+				users.login('matt', 'test', function (success) {
+					if (success) {
+						console.log('logged in!');
+					} else {
+						console.log('login failed');
+					}
+					
+				});
+			});
+		});
+		users.available('matt', null, null, function (success) {
+			if (success) {
+				console.log('users.available failed user check');
+			} else {
+				console.log('users.available passed user check');
+			}
+		});
+	*/
+	users.available('impossiblenonrealuser', 'matt@gmail.com', 'passwird123', function (success) {
+		if (success) {
+			console.log('users.available passed user check');
+		} else {
+			console.log('users.available failed user check');
+		}
+	});
 	//users.create('hipcad', 'mmcwilliams@aspectart.org', 'test1234', function (obj) {
 		//console.dir(obj);
 		users.exists('hipcad', function (exists) {
