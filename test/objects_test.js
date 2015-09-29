152dcbae250b1e134e659adae70b4fcf9a017e2e
Matt <ghost@ghost.local>
2015-09-29 02:13:01 -0400

    Basic CRUD test

--- test/objects_test.js
+++ test/objects_test.js
@@ -1,7 +1,26 @@
-objects.tests = function () {
+var objects = require('../lib/objects.js');
+//objects.tests = function () {
 	//console.time('objects.tests');
 	//objects.index('matt', function (data) {
 		//console.timeEnd('objects.tests');
 	//})
 	//objects.create('matt', 'testobj22', 'This is bulshit source', function () {});
-};
+//};
+
+setTimeout(function () {
+	console.time('took');
+	objects.index('matt', function (data) {
+		console.log(data);
+		objects.create('matt', 'testobj22', 'This is bulshit source', function () {
+			objects.index('matt', function (d) {
+				console.log(d);
+				objects.delete('matt', 'testobj22', function () {
+					objects.index('matt', function (d2) {
+						console.log(d2);
+						console.time('tookEnd');
+					});
+				});
+			});
+		});
+	})
+}, 1000);
