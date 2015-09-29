516b6097781afd31c4f8983cfd742d888a8cf219
Matt <ghost@ghost.local>
2015-09-29 01:46:30 -0400

    Add objects test file

---
+++ test/objects_test.js
@@ -0,0 +1,7 @@
+objects.tests = function () {
+	//console.time('objects.tests');
+	//objects.index('matt', function (data) {
+		//console.timeEnd('objects.tests');
+	//})
+	//objects.create('matt', 'testobj22', 'This is bulshit source', function () {});
+};
