466483e35a56ec51bdbe5fd55f3f3ee71c021546
Matt <dev@Works-Mac-Pro.local>
2015-12-08 13:42:59 -0500

    Start admin file with log reader

---
+++ admin.js
@@ -0,0 +1,37 @@
+var fs = require('fs'),
+	os = require('os'),
+	cmd = require('commander'),
+    moment = require('moment'),
+    path = require('path'),
+    uuid = require('node-uuid'),
+    cfg = require('./lib/cfg.js');
+
+var readLog = function (path) {
+	'use strict';
+	var logRaw = fs.readFileSync(path, 'utf8').split('\n'),
+		log = [],
+		stitch = '',
+		i;
+	for (i = 0; i < logRaw.length; i++) {
+		if (logRaw[i][0] === '{' && logRaw[i][logRaw[i].length - 1] === '}') {
+			log.push(JSON.parse(logRaw[i]));
+		} else {
+			if (stitch === '') {
+				stitch += logRaw[i] + '\n';
+			} else {
+				stitch = logRaw[i];
+				if (logRaw[i][logRaw[i].length - 1] === '}') {
+					log.push(JSON.parse(stitch));
+					stitch = '';
+				} else {
+					stitch += '\n';
+				}
+			}
+		}
+	}
+	return log;
+};
+
+var log = readLog('/var/log/hipcad/db.log');
+
+console.dir(log);
