c8f78c1a85cfd1da9b74e67418840cdcaebb0ea0
mmcwilliams <hi@mmcwilliams.com>
2020-03-17 14:40:29 -0400

    Resolved naming confusion around databases. Module db needed correct package.json name. Installed pg.

--- dist/lib/db/index.js
+++ dist/lib/db/index.js
@@ -1,287 +1,299 @@
-'use strict'
-
-/** @module pg */
+'use strict';
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/** @module db */
 /** General purpose SQL db library for PostgreSQL. Used for prototyping and then deployment. */
-
-let Pool;
-const squelRaw = require('squel');
-const path = require('path');
-const squel = squelRaw.useFlavour('postgres');
-
-const log = require('log')('pg');
-
+const pg_1 = require("pg");
+const path_1 = require("path");
+const squelRaw = __importStar(require("squel"));
+const log = require('log')('db');
 /** Class representing a database connection */
 class DB {
-
-	/**
-	 * Initialized connection to database and create client.
-	 *
-	 * 	@constructor
-	 *  @param 	{String}  	name 	Table name to connect to
-	 */
-	constructor (name, pool) {
-		const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/sandbox'
-		this._tableName = name
-		this.squel = squel
-		if (!pool) {
-			Pool = require('pg').Pool
-			this.pool = new Pool({ connectionString : DATABASE_URL })
-		} else {
-			this.pool = pool; //try this???
-		}
-	}
-
-	async connect () {
-		try {
-			await this._createTable();
-		} catch (err) {
-			log.error(err)
-		}
-	}
-
-	/**
-	 * Create an empty, column-less table and invoke this._describeTable().
-	 *
-	 */
-	async _createTable (silent = true) {
-		const fn = `${this._tableName}._createTable`
-		const create = `CREATE TABLE IF NOT EXISTS ${this._tableName} ();`
-		let res;
-		if (!silent) log.info('DB._createTable', create)
-		try {
-			await this.query(create)
-		} catch (err) {
-			if (err.code === '42P07') {
-				if (!silent) log.info(fn, `TABLE ${this._tableName} already exists`)
-			} else {
-				log.error(fn, err)
-			}
-		}
-		if (!silent && res) log.info(fn, `Created TABLE ${this._tableName}`)
-		await this._describeTable()
-	}
-
-	/**
-	 * Query the table description and build an array of columns to alter.
-	 *
-	 */
-	async _describeTable () {
-		const fn = `${this._tableName}._describeTable`
-		let fullPath = path.join(process.cwd(), `./table/${this._tableName}.json`)
-		const fields = require(fullPath)
-		let alterFields = []
-		const describe = `SElECT column_name, data_type, character_maximum_length FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '${this._tableName}';`
-		let res;
-		try {
-			res = await this.query(describe);
-		} catch (err) {
-			log.error(fn, { err : err })
-		}
-		if (res) {
-			alterFields = this._compareFields(fields, res)
-			await this._alterTable(alterFields)
-		}
-	}
-
-	/**
-	 * Query the table description and build an array of columns to alter.
-	 *
-	 *  @param 	{Array}  master 	Array of columns to compare
-	 *  @param 	{Object} current 	Response from query
-	 */
-	_compareFields (master, current) {
-		let currentMap = {}
-		let masterCols = Object.keys(master)
-		let currentCols = current.rows.map(elem => {
-			currentMap[elem.column_name]
-			return elem.column_name
-		})
-		let inMaster = []
-		let inCurrent = []
-		let toChange = []
-		let alterFields = []
-		for (let col of masterCols) {
-			if (currentCols.indexOf(col) === -1) {
-				inMaster.push(col)
-			}
-		}
-		for (let col of currentCols) {
-			if (masterCols.indexOf(col) === -1) {
-				inCurrent.push(col)
-			}
-		}
-		for (let col of inMaster) {
-			alterFields.push({ add : true, col : col, str : master[col] })
-		}
-		for (let col of inCurrent) {
-			alterFields.push({ drop : true, col : col })
-		}
-		return alterFields
-	}
-
-	/**
-	 * Generate ALTER statement to either ADD or DROP column.
-	 *
-	 *  @param 	{Object}  obj 	Object containing alter information
-	 *
-	 *  @param 	{String} 	Alter table query
-	 */
-	_alterField (obj) {
-		let str
-		if (obj.add) {
-			str = `ALTER TABLE ${this._tableName} ADD COLUMN ${obj.col} ${obj.str}; `
-		} else if (obj.drop) {
-			str = `ALTER TABLE ${this._tableName} DROP COLUMN ${obj.col}; `
-		}
-		return str
-	}
-
-	/**
-	 * Generate and combine all ALTER statements and apply to the 
-	 * table with query.
-	 *
-	 *  @param 	{Array}  fields 	Array of objects from comparing current to master.
-	 */
-	async _alterTable (fields, silent = true) {
-		const fn = `${this._tableName}._alterTable`
-		let alter = '';
-		let res;
-		for (let col of fields) {
-			alter += this._alterField(col)
-		}
-
-		if (alter === '') {
-			if (!silent) log.info(fn, `TABLE ${this._tableName} is up to date`)
-			return true
-		}
-
-		if (!silent) log.info(fn, alter)
-
-		try {
-			res = await this.query(alter)
-		} catch (err) {
-			log.error(fn, err)
-		}
-		let f = fields.map(elem => { return elem.col })
-
-		log.info(fn, `TABLE ${this._tableName} updated columns ${f.join(',')}`)
-	}
-
-	/**
-	 * Drop the table associated with the class.
-	 */
-	_wipeTable () {
-		const fn = `${this._tableName}._wipeTable`
-		const query = `DROP TABLE ${this._tableName};`
-		this.query(query).then((err, res) => {
-			log.info(fn, `TABLE ${this._tableName} wiped`)
-		}).catch(err => {
-			return log.error(fn, { caught : true, err : err })
-		})
-	}
-
-	/**
-	 * Insert record into table associated with class. Returns a promise.
-	 * 
-	 *  @param 	{Object}  obj 	All values to insert
-	 *
-	 *  @return {Function}  Promise of query
-	 */
-	insert (obj, silent = true) {
-		const fn = `${this._tableName}.insert`
-		const query = squel.insert() //{ replaceSingleQuotes: true }
-						.into(this._tableName)
-						.setFields(obj)
-						.toString()
-		if (!silent) {
-			log.info(fn, {query : query })
-		}
-		return this.query(query)
-	}
-
-	/**
-	 * Insert record into table associated with class. Returns a promise.
-	 * 
-	 *  @param  {String}  where WHERE statement to select rows to update
-	 *  @param 	{Object}  obj 	All values to update in row
-	 *
-	 *  @return {Function}  Promise of query
-	 */
-	update (where, obj) {
-		const fn = `${this._tableName}.update`
-		const query = squel.update() //{ replaceSingleQuotes: true }
-						.table(this._tableName)
-						.where(where)
-						.setFields(obj)
-						.toString()
-		log.info(fn, { query : query })
-		return this.query(query)
-	}
-
-	/**
-	 * List all records from table associated with the class.
-	 *
-	 *  @return {Function}  Promise of query
-	 */
-	list () {
-		const fn = `${this._tableName}.list`
-		const query = squel.select()
-						.from(this._tableName)
-						.toString()
-		log.info(fn, { query : query })
-		return this.query(query)
-	}
-
-	/**
-	 * List all records from table associated with the class that match
-	 * the supplied WHERE statement.
-	 *
-	 *  @param  {String}  where 	WHERE statement of rows to return
-	 *
-	 *  @return {Function}  Promise of query
-	 */
-	find (where) {
-		const fn = `${this._tableName}.find`
-		const query = squel.select()
-						.from(this._tableName)
-						.where(where)
-						.toString()
-		log.info(fn, { query : query })
-		return this.query(query)
-	}
-
-	/**
-	 * Open-ended query statment.
-	 *
-	 *  @param  {String}  query 	Query statement to run
-	 *
-	 *  @return {Function}  Promise of query
-	 */
-	query (query) {
-		const fn = `${this._tableName}.query`
-		return new Promise(async (resolve, reject) => {
-			let client;
-			let res;
-			try {
-				client = await this.pool.connect();
-			} catch (err) {
-				return reject(err);
-			}
-			try {
-				res = await client.query(query);
-			} catch (err) {
-				return reject(err);
-			}
-
-			try {
-				await client.release();
-			} catch (err) {
-				return reject(err);
-			}
-
-			return resolve(res);
-		});
-	}
+    /**
+     * Initialized connection to database and create client.
+     *
+     * 	@constructor
+     *  @param 	{String}  	name 	Table name to connect to
+     *  @param 	{Object}	pool 	Pool to connect to, optional.
+     */
+    constructor(name, pool) {
+        const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/sandbox';
+        this._tableName = name;
+        this.squel = squelRaw.useFlavour('postgres');
+        if (!pool) {
+            this._pool = new pg_1.Pool({ connectionString: DATABASE_URL });
+        }
+        else {
+            this._pool = pool;
+        }
+    }
+    /**
+     * Create table if it does not exist. If it does, update table to latest schema.
+     *
+     */
+    async connect() {
+        try {
+            await this._createTable();
+        }
+        catch (err) {
+            //
+        }
+    }
+    /**
+     * Create an empty, column-less table and invoke this._describeTable().
+     *
+     */
+    async _createTable(silent = true) {
+        const fn = `${this._tableName}._createTable`;
+        const create = `CREATE TABLE IF NOT EXISTS ${this._tableName} ();`;
+        let res;
+        if (!silent)
+            log.info('DB._createTable', create);
+        try {
+            res = await this.query(create);
+        }
+        catch (err) {
+            if (err.code === '42P07') {
+                if (!silent)
+                    log.info(fn, `TABLE ${this._tableName} already exists`);
+            }
+            else {
+                log.error(fn, err);
+            }
+        }
+        if (!silent && res)
+            log.info(fn, `Created TABLE ${this._tableName}`);
+        await this._describeTable();
+    }
+    /**
+     * Query the table description and build an array of columns to alter.
+     *
+     */
+    async _describeTable() {
+        const fn = `${this._tableName}._describeTable`;
+        const describe = `SElECT column_name, data_type, character_maximum_length FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '${this._tableName}';`;
+        const fullPath = path_1.join(process.cwd(), `./table/${this._tableName}.json`);
+        const fields = require(fullPath);
+        let alterFields = [];
+        let res;
+        try {
+            res = await this.query(describe);
+        }
+        catch (err) {
+            log.error(fn, err);
+        }
+        if (res) {
+            alterFields = this._compareFields(fields, res);
+            await this._alterTable(alterFields);
+        }
+    }
+    /**
+     * Query the table description and build an array of columns to alter.
+     *
+     *  @param 	{Array}  mast 		Array of columns to compare
+     *  @param 	{Object} current 	Response from query
+     */
+    _compareFields(mast, current) {
+        let currentMap = {};
+        let masterCols = Object.keys(mast);
+        let currentCols = current.rows.map((elem) => {
+            return elem.column_name;
+        });
+        let inMaster = [];
+        let inCurrent = [];
+        let toChange = [];
+        let alterFields = [];
+        for (let col of masterCols) {
+            if (currentCols.indexOf(col) === -1) {
+                inMaster.push(col);
+            }
+        }
+        for (let col of currentCols) {
+            if (masterCols.indexOf(col) === -1) {
+                inCurrent.push(col);
+            }
+        }
+        for (let col of inMaster) {
+            alterFields.push({ add: true, col, str: mast[col] });
+        }
+        for (let col of inCurrent) {
+            alterFields.push({ drop: true, col });
+        }
+        return alterFields;
+    }
+    /**
+     * Generate ALTER statement to either ADD or DROP column.
+     *
+     *  @param 	{Object}  obj 	Object containing alter information
+     *
+     *  @param 	{String} 	Alter table query
+     */
+    _alterField(obj) {
+        let str;
+        if (obj.add) {
+            str = `ALTER TABLE ${this._tableName} ADD COLUMN ${obj.col} ${obj.str}; `;
+        }
+        else if (obj.drop) {
+            str = `ALTER TABLE ${this._tableName} DROP COLUMN ${obj.col}; `;
+        }
+        return str;
+    }
+    /**
+     * Generate and combine all ALTER statements and apply to the
+     * table with query.
+     *
+     *  @param 	{Array}  fields 	Array of objects from comparing current to master.
+     */
+    async _alterTable(fields, silent = true) {
+        const fn = `${this._tableName}._alterTable`;
+        let alter = '';
+        let res;
+        for (let col of fields) {
+            alter += this._alterField(col);
+        }
+        if (alter === '') {
+            if (!silent)
+                log.info(fn, `TABLE ${this._tableName} is up to date`);
+            return;
+        }
+        if (!silent)
+            log.info(fn, alter);
+        try {
+            res = await this.query(alter);
+        }
+        catch (err) {
+            log.error(fn, err);
+        }
+        let f = fields.map((elem) => { return elem.col; });
+        log.info(fn, `TABLE ${this._tableName} updated columns ${f.join(',')}`);
+    }
+    /**
+     * Drop the table associated with the class.
+     */
+    async _wipeTable() {
+        const fn = `${this._tableName}._wipeTable`;
+        const query = `DROP TABLE ${this._tableName};`;
+        let res;
+        try {
+            await this.query(query);
+            log.info(fn, `TABLE ${this._tableName} wiped`);
+        }
+        catch (err) {
+            log.error(fn, err);
+        }
+    }
+    /**
+     * Insert record into table associated with class. Returns a promise.
+     *
+     *  @param 	{Object}  obj 	All values to insert
+     *
+     *  @return {Function}  Promise of query
+     */
+    insert(obj, silent = true) {
+        const fn = `${this._tableName}.insert`;
+        const query = this.squel.insert() //{ replaceSingleQuotes: true }
+            .into(this._tableName)
+            .setFields(obj)
+            .toString();
+        if (!silent) {
+            log.info(fn, query);
+        }
+        return this.query(query);
+    }
+    /**
+     * Insert record into table associated with class. Returns a promise.
+     *
+     *  @param  {String}  where WHERE statement to select rows to update
+     *  @param 	{Object}  obj 	All values to update in row
+     *
+     *  @return {Function}  Promise of query
+     */
+    update(where, obj, silent = true) {
+        const fn = `${this._tableName}.update`;
+        const query = this.squel.update() //{ replaceSingleQuotes: true }
+            .table(this._tableName)
+            .where(where)
+            .setFields(obj)
+            .toString();
+        if (!silent) {
+            log.info(fn, query);
+        }
+        return this.query(query);
+    }
+    /**
+     * List all records from table associated with the class.
+     *
+     *  @return {Function}  Promise of query
+     */
+    list(silent = true) {
+        const fn = `${this._tableName}.list`;
+        const query = this.squel.select()
+            .from(this._tableName)
+            .toString();
+        if (!silent) {
+            log.info(fn, query);
+        }
+        return this.query(query);
+    }
+    /**
+     * List all records from table associated with the class that match
+     * the supplied WHERE statement.
+     *
+     *  @param  {String}  where 	WHERE statement of rows to return
+     *
+     *  @return {Function}  Promise of query
+     */
+    find(where, silent = true) {
+        const fn = `${this._tableName}.find`;
+        const query = this.squel.select()
+            .from(this._tableName)
+            .where(where)
+            .toString();
+        if (!silent) {
+            log.info(fn, query);
+        }
+        return this.query(query);
+    }
+    /**
+     * Open-ended query statment.
+     *
+     *  @param  {String}  query 	Query statement to run
+     *
+     *  @return {Function}  Promise of query
+     */
+    query(query) {
+        const fn = `${this._tableName}.query`;
+        return new Promise(async (resolve, reject) => {
+            let client;
+            let res;
+            try {
+                client = await this._pool.connect();
+            }
+            catch (err) {
+                return reject(err);
+            }
+            try {
+                res = await client.query(query);
+            }
+            catch (err) {
+                client.release(err);
+                return reject(err);
+            }
+            try {
+                await client.release();
+            }
+            catch (err) {
+                return reject(err);
+            }
+            return resolve(res);
+        });
+    }
 }
-
-module.exports = DB
+exports.DB = DB;
+module.exports = DB;
--- dist/lib/dbpool/index.js
+++ dist/lib/dbpool/index.js
@@ -1,23 +1,19 @@
 'use strict';
-
+Object.defineProperty(exports, "__esModule", { value: true });
+const url_1 = require("url");
 const Pool = require('pg-pool');
-const url = require('url');
-
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
-
-const POOL_MAX = process.env.POOL_MAX || 5;
-
+const POOL_MAX = (typeof process.env.POOL_MAX !== 'undefined') ? parseInt(process.env.POOL_MAX, 10) : 5;
+const SSL = (process.env.DATABASE_SSL == '0') ? false : true; //ssl by default
+const PARAMS = url_1.parse(process.env.DATABASE_URL || 'postgres://localhost:5432/sandbox');
+const AUTH = PARAMS.auth.split(':');
 const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  ssl: true,
-  max : POOL_MAX
+    user: AUTH[0],
+    password: AUTH[1],
+    host: PARAMS.hostname,
+    port: PARAMS.port,
+    database: PARAMS.pathname.split('/')[1],
+    ssl: SSL,
+    max: POOL_MAX
 };
-
 const pool = new Pool(config);
-
 module.exports = pool;
