6f8cc4fe09c2d1414bbfc07074cacf9d71bd8223
mattmcw <matt@sixteenmillimeter.com>
2022-05-21 10:29:51 -0400

    Simplify dev script. Do not use process.json with PM2, just use Docker for deployment.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-RED='\033[0;31m'  
-NC='\033[0m' # No Color
+#!/bin/bash
 
-echo "Starting in ${RED}dev${NC} mode"
+echo "Starting in dev mode"
 
-rm run_dev.sh
-jq -r ".apps[0].env | keys[]" ./process.json | while read key ; do
-	/bin/echo -n $key= >> run_dev.sh
-	/bin/echo -n $(jq ".apps[0].env.$key" ./process.json) >> run_dev.sh
-	/bin/echo -n ' '  >> run_dev.sh
-done
-/bin/echo -n " node .">> run_dev.sh
-
-#cat run_dev.sh
-sh run_dev.sh
+node ./dist/index.js --dev
